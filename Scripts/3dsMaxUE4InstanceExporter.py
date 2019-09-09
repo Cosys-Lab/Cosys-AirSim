b97be962f55d022ef12d7c2bf68def0b61987e26
wojansen <wouter.jansen@uantwerpen.be>
2019-09-09 15:29:10 +0200

    Updated 3dsMax Script

--- Scripts/3dsMaxUE4InstanceExporter.py
+++ Scripts/3dsMaxUE4InstanceExporter.py
@@ -63,46 +63,39 @@ class PySideUI(QtWidgets.QDialog):
 		
 		if objectCount is not 0:
 			print("Begin Map\n   Begin Level", file = instancesFile)
-			self.executeInfoLabel.setText("Exporting...\n 0/" + str(objectCount) + " completed.")
+			self.executeInfoLabel.setText("Exporting " + str(objectCount) + " objects...")
 			self.executeInfoLabel.setVisible(1)
 			
 			mySel = MaxPlus.SelectionManager.Nodes
 			index = 0
 			for each in mySel:
 				index = index + 1
-				self.executeInfoLabel.setText("Exporting...\n" + str(index) + "/" + str(objectCount) + " completed.")
 				print("Adding object", each.Name)
 				
-				objName = each.Name
-				objPosX = round(each.Position.GetX(), 2) + 0
-				objPosY = round(each.Position.GetY() * -1, 2) + 0
-				objPosZ = round(each.Position.GetZ(), 2) + 0
-				objRotX = round(each.Rotation.GetEuler().GetY() * -1 * 180 / math.pi , 4) + 0
-				objRotY = round(each.Rotation.GetEuler().GetZ() * -1 * 180 / math.pi , 4) + 0
-				objRotZ = round(each.Rotation.GetEuler().GetX() * 180 /  math.pi , 4) + 0
+				scaledMatrix = each.Transform
+				setToScale = MaxPlus.Point3(1,1,1) / scaledMatrix.Scale
+				scaledMatrix.PreScale(setToScale)
 				
-				print("      Begin Actor Class=StaticMeshActor Name={} Archetype=StaticMeshActor'/Script/Engine.Default__StaticMeshActor'".format(objName.encode("utf-8")), file = instancesFile)
+				print("      Begin Actor Class=StaticMeshActor Name={} Archetype=StaticMeshActor'/Script/Engine.Default__StaticMeshActor'".format(each.Name.encode("utf-8")), file = instancesFile)
 				print("         Begin Object Class=StaticMeshComponent Name=StaticMeshComponent0 ObjName=StaticMeshComponent0 Archetype=StaticMeshComponent'/Script/Engine.Default__StaticMeshActor:StaticMeshComponent0'\n         End Object", file = instancesFile)
 				print("         Begin Object Name=StaticMeshComponent0\n            StaticMesh=StaticMesh'/Engine/EditorMeshes/EditorCube.EditorCube'", file = instancesFile)
-				print("            RelativeLocation=(X={},Y={},Z={})".format(objPosX, objPosY, objPosZ), file = instancesFile)
+				print("            RelativeLocation=(X={},Y={},Z={})".format(round(each.Position.GetX(), 2) + 0, round(each.Position.GetY() * -1, 2) + 0, round(each.Position.GetZ(), 2) + 0), file = instancesFile)
 				print("            RelativeScale3D=(X=1.0,Y=1.0,Z=1.0)", file = instancesFile)
-				print("            RelativeRotation=(Pitch={},Yaw={},Roll={})".format(objRotX, objRotY, objRotZ), file = instancesFile)
+				print("            RelativeRotation=(Pitch={},Yaw={},Roll={})".format(round(scaledMatrix.Rotation.GetEuler().GetY() * -1 * 180 / math.pi , 4) + 0, round(scaledMatrix.Rotation.GetEuler().GetZ() * -1 * 180 / math.pi , 4) + 0, round(scaledMatrix.Rotation.GetEuler().GetX() * 180 /  math.pi , 4) + 0), file = instancesFile)
 				print("            CustomProperties \n         End Object\n         StaticMeshComponent=StaticMeshComponent0\n         Components(0)=StaticMeshComponent0\n         RootComponent=StaticMeshComponent0", file = instancesFile)
-				print("         ActorLabel=\"{}\"".format(objName.encode("utf-8")), file = instancesFile)
+				print("         ActorLabel=\"{}\"".format(each.Name.encode("utf-8")), file = instancesFile)
 				print("      End Actor", file = instancesFile)				
 				print("Added!")
-
 				
 			print("   End Level\nBegin Surface\nEnd Surface\nEnd Map", file = instancesFile)
 			print("Export completed!")
-			self.executeInfoLabel.setText("Export completed! "  + str(index) + "/" + str(objectCount) + "\nYou can now open the instances.txt file,\ncopy all the text to the clipboard and paste it in an Unreal 4 editor to spawn cubes on the object locations.\nWhich you can then replace with the instanced models that are imported separately.")
+			self.executeInfoLabel.setText("Export completed for " + str(objectCount) + " objects!\nYou can now open the instances.txt file,\ncopy all the text to the clipboard and paste it in an Unreal 4 editor to spawn cubes on the object locations.\nWhich you can then replace with the instanced models that are imported separately.")
 			subprocess.Popen("explorer /select," + self.folderTextField.text()+ "\instances.txt")
 			
 		else:
 			print("Nothing Selected!")
 			self.executeInfoLabel.setVisible(1)
 			self.executeInfoLabel.setText("No objects selected!")
-
 
 	except Exception, e:
 		print("Error: ",e)
