rajat2004/AirSim

Add getCameraInfo API for external cameras

AirLib/src/api/RpcLibClientBase.cpp
@@ -376,9 +376,9 @@ bool RpcLibClientBase::simSetObjectScale(const std::string& object_name, const m
 	return pimpl_->client.call("simSetObjectScale", object_name, RpcLibAdapatorsBase::Vector3r(scale)).as<bool>();
 }
 
-CameraInfo RpcLibClientBase::simGetCameraInfo(const std::string& camera_name, const std::string& vehicle_name) const
+CameraInfo RpcLibClientBase::simGetCameraInfo(const std::string& camera_name, const std::string& vehicle_name, bool external) const
 {
-    return pimpl_->client.call("simGetCameraInfo", camera_name, vehicle_name).as<RpcLibAdapatorsBase::CameraInfo>().to();
+    return pimpl_->client.call("simGetCameraInfo", camera_name, vehicle_name, external).as<RpcLibAdapatorsBase::CameraInfo>().to();
 }
 
 void RpcLibClientBase::simSetCameraPose(const std::string& camera_name, const Pose& pose, const std::string& vehicle_name)

AirLib/src/api/RpcLibServerBase.cpp
@@ -233,8 +233,9 @@ RpcLibServerBase::RpcLibServerBase(ApiProvider* api_provider, const std::string&
         return RpcLibAdapatorsBase::DistanceSensorData(distance_sensor_data);
     });
 
-    pimpl_->server.bind("simGetCameraInfo", [&](const std::string& camera_name, const std::string& vehicle_name) -> RpcLibAdapatorsBase::CameraInfo {
-        const auto& camera_info = getVehicleSimApi(vehicle_name)->getCameraInfo(camera_name);
+    pimpl_->server.bind("simGetCameraInfo", [&](const std::string& camera_name, const std::string& vehicle_name, bool external) -> RpcLibAdapatorsBase::CameraInfo {
+        // const auto& camera_info = getVehicleSimApi(vehicle_name)->getCameraInfo(camera_name);
+        const auto& camera_info = getWorldSimApi()->getCameraInfo(camera_name, vehicle_name, external);
         return RpcLibAdapatorsBase::CameraInfo(camera_info);
     });
 

Cosys-Lab/Cosys-AirSim

AirLib/src/api/RpcLibClientBase.cpp
@@ -435,9 +435,9 @@ __pragma(warning(disable : 4239))
             return pimpl_->client.call("simSetObjectScale", object_name, RpcLibAdaptorsBase::Vector3r(scale)).as<bool>();
         }
 
-        CameraInfo RpcLibClientBase::simGetCameraInfo(const std::string& camera_name, const std::string& vehicle_name) const
+        CameraInfo RpcLibClientBase::simGetCameraInfo(const std::string& camera_name, const std::string& vehicle_name, bool external) const
         {
-            return pimpl_->client.call("simGetCameraInfo", camera_name, vehicle_name).as<RpcLibAdaptorsBase::CameraInfo>().to();
+            return pimpl_->client.call("simGetCameraInfo", camera_name, vehicle_name, external).as<RpcLibAdaptorsBase::CameraInfo>().to();
         }
 
         void RpcLibClientBase::simSetCameraPose(const std::string& camera_name, const Pose& pose, const std::string& vehicle_name)

AirLib/src/api/RpcLibServerBase.cpp
@@ -268,8 +268,8 @@ namespace airlib
             return RpcLibAdaptorsBase::DistanceSensorData(distance_sensor_data);
         });
 
-        pimpl_->server.bind("simGetCameraInfo", [&](const std::string& camera_name, const std::string& vehicle_name) -> RpcLibAdaptorsBase::CameraInfo {
-            const auto& camera_info = getVehicleSimApi(vehicle_name)->getCameraInfo(camera_name);
+        pimpl_->server.bind("simGetCameraInfo", [&](const std::string& camera_name, const std::string& vehicle_name, bool external) -> RpcLibAdaptorsBase::CameraInfo {
+            const auto& camera_info = getWorldSimApi()->getCameraInfo(camera_name, vehicle_name, external);
             return RpcLibAdaptorsBase::CameraInfo(camera_info);
         });
 