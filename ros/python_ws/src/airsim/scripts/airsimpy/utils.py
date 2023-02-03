9c99ba4a8739c2b7da32b04acf59ff9d5d27c7f2
Wouter Jansen <wouter.jansen@uantwerpen.be>
2023-02-03 16:00:15 +0100

    readability improved

--- ros/python_ws/src/airsim/scripts/airsimpy/utils.py
+++ ros/python_ws/src/airsim/scripts/airsimpy/utils.py
@@ -160,12 +160,11 @@ def to_eularian_angles(q):
     y = q.y_val
     x = q.x_val
     w = q.w_val
-    ysqr = y * y
 
     # roll (x-axis rotation)
-    t0 = +2.0 * (w*x + y*z)
-    t1 = +1.0 - 2.0*(x*x + ysqr)
-    roll = math.atan2(t0, t1)
+    sinr_cosp  = +2.0 * (w*x + y*z)
+    cosr_cosp = +1.0 - 2.0*(x*x + y*y)
+    roll = math.atan2(sinr_cosp, cosr_cosp)
 
     # pitch (y-axis rotation)
     t2 = +2.0 * (w*y - z*x)
@@ -176,26 +175,26 @@ def to_eularian_angles(q):
     pitch = math.asin(t2)
 
     # yaw (z-axis rotation)
-    t3 = +2.0 * (w*z + x*y)
-    t4 = +1.0 - 2.0 * (ysqr + z*z)
-    yaw = math.atan2(t3, t4)
+    siny_cosp = +2.0 * (w*z + x*y)
+    cosy_cosp = +1.0 - 2.0 * (y*y + z*z)
+    yaw = math.atan2(siny_cosp, cosy_cosp)
 
-    return (pitch, roll, yaw)
+    return pitch, roll, yaw
 
     
 def to_quaternion(pitch, roll, yaw):
-    t0 = math.cos(yaw * 0.5)
-    t1 = math.sin(yaw * 0.5)
-    t2 = math.cos(roll * 0.5)
-    t3 = math.sin(roll * 0.5)
-    t4 = math.cos(pitch * 0.5)
-    t5 = math.sin(pitch * 0.5)
+    cy = math.cos(yaw * 0.5)
+    sy = math.sin(yaw * 0.5)
+    cr = math.cos(roll * 0.5)
+    sr = math.sin(roll * 0.5)
+    cp = math.cos(pitch * 0.5)
+    sp = math.sin(pitch * 0.5)
 
     q = Quaternionr()
-    q.w_val = t0 * t2 * t4 + t1 * t3 * t5 #w
-    q.x_val = t0 * t3 * t4 - t1 * t2 * t5 #x
-    q.y_val = t0 * t2 * t5 + t1 * t3 * t4 #y
-    q.z_val = t1 * t2 * t4 - t0 * t3 * t5 #z
+    q.w_val = cy * cr * cp + sy * sr * sp #w
+    q.x_val = cy * sr * cp - sy * cr * sp #x
+    q.y_val = cy * cr * sp + sy * sr * cp #y
+    q.z_val = sy * cr * cp - cy * sr * sp #z
     return q
 
     
