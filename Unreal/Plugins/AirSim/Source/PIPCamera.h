8e4184f325cb8e2d5321eb45ca2948f4fdcb3b4d
Wouter Jansen <wouter.jansen@uantwerpen.be>
2024-04-11 09:03:51 +0200

    more fixes

--- Unreal/Plugins/AirSim/Source/PIPCamera.h
+++ Unreal/Plugins/AirSim/Source/PIPCamera.h
@@ -14,7 +14,8 @@
 //CinemAirSim
 #include <CineCameraActor.h>
 #include <CineCameraComponent.h>
-
+#include "RunTime/CinematicCamera/Public/CineCameraComponent.h"
+#include "RunTime/CinematicCamera/Public/CineCameraActor.h"
 #include "Materials/MaterialParameterCollection.h"
 #include "Materials/MaterialParameterCollectionInstance.h"
 #include "Materials/MaterialInstanceDynamic.h"
