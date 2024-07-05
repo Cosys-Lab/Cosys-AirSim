161fd4b5e9ad87bce1665960ebf5b2a124e84d16
Wouter Jansen <wouter.jansen@uantwerpen.be>
2024-07-05 13:32:50 +0200

    Added GPULidar support (adds PCL (conversion) to dependencies) and Echo/Lidar groundtruth label support with new message type StringArray

--- ros2/src/airsim_ros_pkgs/src/airsim_node.cpp
+++ ros2/src/airsim_ros_pkgs/src/airsim_node.cpp
@@ -9,6 +9,8 @@ int main(int argc, char** argv)
     std::shared_ptr<rclcpp::Node> nh = rclcpp::Node::make_shared("airsim_node", node_options);
     std::shared_ptr<rclcpp::Node> nh_img = nh->create_sub_node("img");
     std::shared_ptr<rclcpp::Node> nh_lidar = nh->create_sub_node("lidar");
+    std::shared_ptr<rclcpp::Node> nh_gpulidar = nh->create_sub_node("gpulidar");
+    std::shared_ptr<rclcpp::Node> nh_echo = nh->create_sub_node("echo");
     std::string host_ip;
     uint16_t host_port = 41451;
     bool enable_api_control = false;
@@ -16,7 +18,7 @@ int main(int argc, char** argv)
     nh->get_parameter("host_port", host_port);
     nh->get_parameter("enable_api_control", enable_api_control);
     auto callbackGroup = nh->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
-    AirsimROSWrapper airsim_ros_wrapper(nh, nh_img, nh_lidar, host_ip, callbackGroup, enable_api_control, host_port);
+    AirsimROSWrapper airsim_ros_wrapper(nh, nh_img, nh_lidar, nh_gpulidar, nh_echo, host_ip, callbackGroup, enable_api_control, host_port);
 
     rclcpp::executors::MultiThreadedExecutor executor;
     executor.add_node(nh);
