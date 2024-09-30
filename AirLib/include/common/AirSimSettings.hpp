78fba44e637c81185f3802eba20d83fab90fc363
Wouter Jansen <wouter.jansen@uantwerpen.be>
2024-09-30 16:06:06 +0200

    Added main parameter to toggle initial instance segmentation. Set to true by default. Ref #23
    (cherry picked from commit fe0a647f76194f9a9290e7cd103271e4471fd490)

--- AirLib/include/common/AirSimSettings.hpp
+++ AirLib/include/common/AirSimSettings.hpp
@@ -509,6 +509,7 @@ namespace airlib
         float clock_speed = 1.0f;
         bool engine_sound = false;
         bool move_world_origin = false;
+        bool initial_instance_segmentation = true;
         bool log_messages_visible = true;
         bool show_los_debug_lines_ = false;
         HomeGeoPoint origin_geopoint{ GeoPoint(47.641468, -122.140165, 122) }; //The geo-coordinate assigned to Unreal coordinate 0,0,0
@@ -1454,6 +1455,7 @@ namespace airlib
             speed_unit_factor = settings_json.getFloat("SpeedUnitFactor", 1.0f);
             speed_unit_label = settings_json.getString("SpeedUnitLabel", "m\\s");
             move_world_origin = settings_json.getBool("MoveWorldOrigin", false);
+            initial_instance_segmentation = settings_json.getBool("InitialInstanceSegmentation", true);
             log_messages_visible = settings_json.getBool("LogMessagesVisible", true);
             show_los_debug_lines_ = settings_json.getBool("ShowLosDebugLines", false);
 
