ba998c1e92724014b540f054e049fb77dc0a8cda
Wouter Jansen <wouter.jansen@uantwerpen.be>
2024-10-01 14:07:48 +0200

    Preparation for fixing up docker
    (cherry picked from commit 7a46969e7d29414029eae42329e9191a51cbb476)

--- docker/download_blocks_env_binary.sh
+++ docker/download_blocks_env_binary.sh
@@ -5,6 +5,6 @@ if ! which unzip; then
     sudo apt-get install unzip
 fi
 
-wget -c https://github.com/microsoft/AirSim/releases/download/v1.6.0-linux/Blocks.zip 
-unzip -q Blocks.zip
-rm Blocks.zip
+wget -c https://github.com/Cosys-Lab/Cosys-AirSim/releases/download/5.4-v3.1/Blocks_package_54_31.zip
+unzip -q Blocks_package_54_31.zip
+rm Blocks_package_54_31.zip
