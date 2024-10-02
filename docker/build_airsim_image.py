20bbc64370e263a4d9bc6f737c6e90f1ea84786c
Wouter Jansen <wouter.jansen@uantwerpen.be>
2024-10-02 11:18:17 +0200

    work on docker

--- docker/build_airsim_image.py
+++ docker/build_airsim_image.py
@@ -14,7 +14,7 @@ def build_docker_image(args):
     dockerfile = 'Dockerfile_source'
     if args.source:
         if not args.base_image:
-            args.base_image = "ghcr.io/epicgames/unreal-engine:runtime"
+            args.base_image = "ghcr.io/epicgames/unreal-engine:dev-5.4.3"
         target_image_tag = args.base_image.split(":")[1] # take tag from base image
         if not args.target_image:
             args.target_image = 'airsim_source' + ':' + target_image_tag
@@ -22,7 +22,7 @@ def build_docker_image(args):
     else:
         dockerfile = 'Dockerfile_binary'
         if not args.base_image:
-            args.base_image = "ghcr.io/epicgames/unreal-engine:dev-5.4.3"
+            args.base_image = "ghcr.io/epicgames/unreal-engine:runtime-pixel-streaming"
         target_image_tag = args.base_image.split(":")[1] # take tag from base image
         if not args.target_image:
             args.target_image = 'airsim_binary' + ':' + target_image_tag
