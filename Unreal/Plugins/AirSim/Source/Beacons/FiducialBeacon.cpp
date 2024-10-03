fe4be7110a4a82807b5eb72843883a5e9efddcdf
Wouter Jansen <wouter.jansen@uantwerpen.be>
2024-10-03 16:18:07 +0200

    moved pragma for disabling optimization here

--- Unreal/Plugins/AirSim/Source/Beacons/FiducialBeacon.cpp
+++ Unreal/Plugins/AirSim/Source/Beacons/FiducialBeacon.cpp
@@ -1,5 +1,4 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-#pragma warning(disable: 4883)
 
 #include "FiducialBeacon.h"
 #include "UObject/ConstructorHelpers.h"
@@ -7,6 +6,7 @@
 #include "Materials/MaterialInstanceDynamic.h"
 
 // Sets default values
+#pragma warning(disable: 4883)
 AFiducialBeacon::AFiducialBeacon()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
