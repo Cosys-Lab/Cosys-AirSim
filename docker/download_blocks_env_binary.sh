c4fe5576763994de41ad9dba7557ad93575d3327
Wouter J. <WouterJansen@users.noreply.github.com>
2025-10-03 13:47:38 +0200

    Update download link for Blocks packaged zip

--- docker/download_blocks_env_binary.sh
+++ docker/download_blocks_env_binary.sh
@@ -5,7 +5,7 @@ if ! which unzip; then
     sudo apt-get install unzip
 fi
 
-wget -c https://github.com/Cosys-Lab/Cosys-AirSim/releases/download/5.6-v3.4/Blocks_packaged_Linux_55_33.zip
+wget -c https://github.com/Cosys-Lab/Cosys-AirSim/releases/download/5.5-v3.3/Blocks_packaged_Linux_55_33.zip
 unzip -q Blocks_packaged_Linux_55_33.zip
 rm Blocks_packaged_Linux_55_33.zip
 mv Blocks_packaged_Linux_55_33 LinuxBlocks
