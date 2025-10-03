9ff4f313385492ca081286c4f01ef057ce0dc8bb
Wouter J. <WouterJansen@users.noreply.github.com>
2025-10-03 13:28:21 +0200

    Add files via upload

--- docker/download_blocks_env_binary.sh
+++ docker/download_blocks_env_binary.sh
@@ -5,6 +5,7 @@ if ! which unzip; then
     sudo apt-get install unzip
 fi
 
-wget -c https://github.com/Cosys-Lab/Cosys-AirSim/releases/download/5.5-v3.2/Blocks_packaged_Linux_55_32.zip
-unzip -q Blocks_packaged_Linux_55_32.zip
-rm Blocks_packaged_Linux_55_32.zip
+wget -c https://github.com/Cosys-Lab/Cosys-AirSim/releases/download/5.6-v3.4/Blocks_packaged_Linux_56_34.zip
+unzip -q Blocks_packaged_Linux_56_34.zip
+rm Blocks_packaged_Linux_56_34.zip
+mv Blocks_packaged_Linux_56_34 LinuxBlocks
