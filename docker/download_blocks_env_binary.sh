43932d367b82f294fa69d8521024fbcd7eab7fcd
Wouter J. <WouterJansen@users.noreply.github.com>
2025-10-03 10:52:03 +0200

    Rename extracted blocks directory to LinuxBlocks

--- docker/download_blocks_env_binary.sh
+++ docker/download_blocks_env_binary.sh
@@ -8,3 +8,4 @@ fi
 wget -c https://github.com/Cosys-Lab/Cosys-AirSim/releases/download/5.4-v3.2/Blocks_packaged_Linux_54_32.zip
 unzip -q Blocks_packaged_Linux_54_32.zip
 rm Blocks_packaged_Linux_54_32.zip
+mv Blocks_packaged_Linux_55_32 LinuxBlocks
